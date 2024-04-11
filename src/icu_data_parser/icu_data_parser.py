4cd784cfddb78e34810c8b1af6a3b14e025089f2
Paul Isaris <paulisaris@gmail.com>
2024-04-11 16:39:05 +0300

    - Added the functionality to parse the files and create a final .csv - Added some entries to .gitignore

--- src/icu_data_parser/icu_data_parser.py
+++ src/icu_data_parser/icu_data_parser.py
@@ -1,87 +1,71 @@
 import os
+import csv
 
 """
     A class used to parse ICU data files, and produce a final dataset file.
 """
 
 
+def non_blank_lines(f):
+    for line in f:
+        line = line.rstrip()
+        if line:
+            yield line
+
+
 class ICUDataParser:
     def __init__(self, logger):
         self.logger = logger
+        # final data list to store the final dataset
+        self.final_data = []
+        # add the final data columns (headers) here
+        headers = ["patient_id", "timestamp", "cpp", "glucose", "haemoglobin", "heart_rate", "icp",
+                   "mean_blood_pressure", "paco2", "pao2", "peep", "ph", "respiration_rate", "spo2",
+                   "temperature"]
+        self.final_data.append(headers)
+        self.column_index = {header: index for index, header in enumerate(headers)}
+
+    def add_data_value_for_patient_at_timestamp(self, patient_id, timestamp, data_type, value):
+        # find the patient in the final data list
+        for data in self.final_data:
+            if data[:2] == [patient_id, timestamp]:
+                data[self.column_index[data_type]] = value
+                return
+
+        # if the patient/timestamp record does not exist, we need to add the new record
+
+        # Create a new list with all elements being -1
+        new_element = [-1] * len(self.final_data[0])
+        # Set patient_id and the timestamp
+        new_element[self.column_index["patient_id"]] = patient_id
+        new_element[self.column_index["timestamp"]] = timestamp
+
+        # Set the value of the desired column
+        new_element[self.column_index[data_type]] = value
+        # Append the new element to final_data
+        self.final_data.append(new_element)
 
     def parse(self, files):
         self.logger.debug(f"Found {len(files)} files to parse")
         for file in files:
             file_name = os.path.basename(file)
             self.logger.debug(f"Parsing file {file_name} ...")
-            # Add more elif conditions here for other file types
-            if file_name == "CPP.txt":
-                self.parse_cpp_file(file)
-            elif file_name == "episodes with high icp.txt":
-                self.parse_ep_high_icp_file(file)
-            elif file_name == "Glucose.txt":
-                self.parse_glucose_file(file)
-            elif file_name == "Hemoglobin.txt":
-                self.parse_hemoglobin_file(file)
-            elif file_name == "Hear rate.txt":
-                self.parse_heart_rate_file(file)
-            elif file_name == "ICP.txt":
-                self.parse_icp_file(file)
-            elif file_name == "Mean blood pressure.txt":
-                self.parse_mean_blood_pressure_file(file)
-            elif file_name == "PaCO2.txt":
-                self.parse_paco2_file(file)
-            elif file_name == "PaO2.txt":
-                self.parse_pao2_file(file)
-            elif file_name == "PEEP.txt":
-                self.parse_peep_file(file)
-            elif file_name == "PH.txt":
-                self.parse_ph_file(file)
-            elif file_name == "Respiration rate.txt":
-                self.parse_respiration_rate_file(file)
-            elif file_name == "SpO2.txt":
-                self.parse_spo2_file(file)
-            elif file_name == "Temperature.txt":
-                self.parse_temperature_file(file)
+            if file_name.endswith(".txt"):
+                self.parse_file(file, file_name[:-4].lower().replace(" ", "_"))
+        # save the final data to a file
+        self.save_final_data()
 
-    def parse_cpp_file(self, file):
-        pass
+    def parse_file(self, file, column_name):
+        with open(file, 'r') as f:
+            for line in non_blank_lines(f):
+                print(line)
+                patient_id, timestamp, _, value = line.split(",")
+                patient_id = patient_id.strip().replace('"', "")
+                timestamp = timestamp.strip().replace('"', "")
+                value = value.strip().replace('"', "")
+                self.add_data_value_for_patient_at_timestamp(patient_id, timestamp, column_name, value)
 
-    def parse_ep_high_icp_file(self, file):
-        pass
-
-    def parse_glucose_file(self, file):
-        pass
-
-    def parse_hemoglobin_file(self, file):
-        pass
-
-    def parse_heart_rate_file(self, file):
-        pass
-
-    def parse_icp_file(self, file):
-        pass
-
-    def parse_mean_blood_pressure_file(self, file):
-        pass
-
-    def parse_paco2_file(self, file):
-        pass
-
-    def parse_pao2_file(self, file):
-        pass
-
-    def parse_peep_file(self, file):
-        pass
-
-    def parse_ph_file(self, file):
-        pass
-
-    def parse_respiration_rate_file(self, file):
-        pass
-
-    def parse_spo2_file(self, file):
-        pass
-
-    def parse_temperature_file(self, file):
-        pass
+    def save_final_data(self, filename='final_data.csv'):
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(self.final_data)
