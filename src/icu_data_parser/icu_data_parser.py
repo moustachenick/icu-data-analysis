04aa791241f691dc74ee15718dcc9044c61ae6bf
Paul Isaris <paulisaris@gmail.com>
2024-04-18 15:58:10 +0300

    Updated the data parser to compute the average value of each value in each timestamp

--- src/icu_data_parser/icu_data_parser.py
+++ src/icu_data_parser/icu_data_parser.py
@@ -1,16 +1,10 @@
 import os
 import csv
+from datetime import datetime, timedelta
 
 """
     A class used to parse ICU data files, and produce a final dataset file.
 """
-
-
-def non_blank_lines(f):
-    for line in f:
-        line = line.rstrip()
-        if line:
-            yield line
 
 
 class ICUDataParser:
@@ -18,6 +12,40 @@ class ICUDataParser:
         self.logger = logger
         # final data list to store the final dataset
         self.final_data = []
+
+        """
+        patient data map to store the timestamp and values for each column of patient
+        each of the map items will look like:
+        
+        {
+            "458":{
+               "values":{
+                  "2012-10-09 06:00:00":{
+                     "glucose":{
+                        "value":"100.0",
+                        "num_records_processed":3
+                     },
+                     "haemoglobin":{
+                        "value":"12.8",
+                        "num_records_processed":1
+                     },
+                  }
+                  "2012-10-09 06:30:00":{
+                     "glucose":{
+                        "value":"110.0",
+                        "num_records_processed":2
+                     },
+                     "haemoglobin":{
+                        "value":"10.4",
+                        "num_records_processed":3
+                     },
+                  }
+               }
+               "date_of_birth": "1989-01-01"
+            }
+        }
+        """
+        self.patient_data_map = {}
         # add the final data columns (headers) here
         headers = [
             "patient_id",
@@ -40,7 +68,7 @@ class ICUDataParser:
         self.final_data.append(headers)
         self.column_index = {header: index for index, header in enumerate(headers)}
 
-    def parse(self, files):
+    def parse(self, files, data_dir):
         self.logger.debug(f"Found {len(files)} files to parse")
         for file in files:
             file_name = os.path.basename(file)
@@ -49,16 +77,25 @@ class ICUDataParser:
                 self.parse_episodes_with_high_icp(file)
             elif file_name.endswith(".txt"):
                 self.parse_file(file, file_name[:-4].lower().replace(" ", "_"))
+        # Create the final data
+        self.create_final_data()
         # save the final data to a file
-        self.save_final_data()
+        self.save_final_data(data_dir)
 
     def parse_file(self, file, column_name):
         with open(file, "r", encoding='utf-8-sig') as f:
-            for line in non_blank_lines(f):
+            for line in Helpers.non_blank_lines(f):
                 self.logger.debug(f"Processing line: {line}")
                 patient_id, timestamp, _, value = line.split(",")
                 patient_id = patient_id.strip().replace('"', "")
                 timestamp = timestamp.strip().replace('"', "")
+
+                # Convert the timestamp string to a datetime object
+                timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+
+                # Round the timestamp to the nearest half hour
+                timestamp = Helpers.round_time(timestamp)
+
                 value = value.strip().replace('"', "")
                 self.add_data_value_for_patient_at_timestamp(
                     patient_id, timestamp, column_name, value
@@ -66,7 +103,7 @@ class ICUDataParser:
 
     def parse_episodes_with_high_icp(self, file):
         with open(file, "r", encoding='utf-8-sig') as f:
-            for line in non_blank_lines(f):
+            for line in Helpers.non_blank_lines(f):
                 self.logger.debug(f"Processing line: {line}")
                 patient_id, date_of_birth, _, _, _, _, _, _ = line.split(",")
                 patient_id = patient_id.strip().replace('"', "")
@@ -74,37 +111,79 @@ class ICUDataParser:
                 self.add_date_of_birth_for_patient(patient_id, date_of_birth)
 
     def add_data_value_for_patient_at_timestamp(
-        self, patient_id, timestamp, data_type, value
+            self, patient_id, timestamp, data_type, value
     ):
-        # find the patient in the final data list
-        for data in self.final_data:
-            if (
-                data[self.column_index["patient_id"]] == patient_id
-                and data[self.column_index["timestamp"]] == timestamp
-            ):
-                data[self.column_index[data_type]] = value
-                return
+        # check if the patient exists in the map
+        if patient_id not in self.patient_data_map:
+            self.patient_data_map[patient_id] = {
+                "values": {},
+            }
 
-        # if the patient/timestamp record does not exist, we need to add the new record
+        # check if the timestamp exists for the patient in the map
+        if timestamp not in self.patient_data_map[patient_id]["values"]:
+            self.patient_data_map[patient_id]["values"][timestamp] = {}
 
-        # Create a new list with all elements being -1
-        new_element = [-1] * len(self.final_data[0])
-        # Set patient_id and the timestamp
-        new_element[self.column_index["patient_id"]] = patient_id
-        new_element[self.column_index["timestamp"]] = timestamp
-
-        # Set the value of the desired column
-        new_element[self.column_index[data_type]] = value
-        # Append the new element to final_data
-        self.final_data.append(new_element)
+        # check if the data type exists for the timestamp of the patient in the map
+        if data_type not in self.patient_data_map[patient_id]["values"][timestamp]:
+            self.patient_data_map[patient_id]["values"][timestamp][data_type] = {
+                'value': value,
+                'num_records_processed': 1
+            }
+        else:
+            # if the data type already exists, update the value with the average and increment the count
+            current_value = self.patient_data_map[patient_id]["values"][timestamp][data_type]['value']
+            num_of_records_processed = self.patient_data_map[patient_id]["values"][timestamp][data_type][
+                                           'num_records_processed'] + 1
+            average_value = float(current_value) + (float(value) - float(current_value)) / num_of_records_processed
+            self.patient_data_map[patient_id]["values"][timestamp][data_type]['value'] = average_value
+            self.patient_data_map[patient_id]["values"][timestamp][data_type][
+                'num_records_processed'] = num_of_records_processed
 
     def add_date_of_birth_for_patient(self, patient_id, date_of_birth):
-        # find the patient in the final data list
-        for data in self.final_data:
-            if data[:1] == [patient_id]:
-                data[self.column_index["date_of_birth"]] = date_of_birth
+        # check if the patient exists in the map
+        if patient_id not in self.patient_data_map:
+            self.patient_data_map[patient_id] = {}
 
-    def save_final_data(self, filename="final_data.csv"):
-        with open(filename, "w", newline="") as f:
+        # add the date of birth to the patient data in the map
+        self.patient_data_map[patient_id]["date_of_birth"] = date_of_birth
+
+    def create_final_data(self):
+        # Iterate over the patient_data_map
+        for patient_id, patient_data in self.patient_data_map.items():
+            for timestamp, data_types in patient_data["values"].items():
+                # Create a new row for each timestamp, initialize it with -1
+                row = [-1] * len(self.final_data[0])
+                row[self.column_index['patient_id']] = patient_id
+                row[self.column_index['timestamp']] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+                # Add the data values for each data type
+                for data_type, data in data_types.items():
+                    row[self.column_index[data_type]] = data['value']
+                # Add the date of birth
+                row[self.column_index['date_of_birth']] = patient_data['date_of_birth']
+                # Add the row to the final_data list
+                self.final_data.append(row)
+
+    def save_final_data(self, data_dir, file_name="final_data.csv"):
+        file = data_dir + os.sep + file_name
+        with open(file, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerows(self.final_data)
+
+
+class Helpers:
+    @staticmethod
+    def round_time(dt, minutes=30):
+        """
+        Rounds a datetime object to its nearest half hour.
+        """
+        round_to = timedelta(minutes=minutes)
+        seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+        rounding = (seconds + round_to.seconds / 2) // round_to.seconds * round_to.seconds
+        return dt + timedelta(0, rounding - seconds, -dt.microsecond)
+
+    @staticmethod
+    def non_blank_lines(f):
+        for line in f:
+            line = line.rstrip()
+            if line:
+                yield line
